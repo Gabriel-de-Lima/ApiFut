38295170a4f9bb00746f8ce6b75858600c6c6142
Daniel José Martins Junior <daniel.jr2501@gmail.com>
2024-07-29 15:26:21 -0300

    Melhorias no design da tela inicial

--- app/src/main/java/com/example/apifut/ui/views/PlayerListScreen.kt
+++ app/src/main/java/com/example/apifut/ui/views/PlayerListScreen.kt
@@ -1,8 +1,11 @@
 package com.example.apifut.ui.views
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,10 +18,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -34,7 +40,9 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -108,19 +116,33 @@ fun PlayerList(
 ) {
     Column(
         modifier = Modifier
-            .background(Color.LightGray)
+            .background(Color(218, 250, 218))
             .fillMaxSize()
     ) {
-        Text(
-            text = "Player List",
-            style = MaterialTheme.typography.headlineMedium,
+        Row(
             modifier = Modifier
-                .padding(16.dp)
-                .background(Color.Blue)
                 .fillMaxWidth()
-                .padding(8.dp),
-            color = Color.White
-        )
+                .background(Color(0, 64, 0))
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_app),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(top = 3.dp, bottom = 3.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "Player List FC 24",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
         LazyColumn {
             items(players) { player ->
                 PlayerEntry(player = player, navController = navController)
@@ -138,9 +160,16 @@ fun PlayerEntry(
     val authToken = "820c26f3-2944-4dea-8260-b5a59fa5faac"
 
     Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
         modifier = Modifier
             .padding(6.dp)
             .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = Color(0, 64, 0),
+                RoundedCornerShape(corner = CornerSize(15.dp)))
             .clickable {
                 Log.d("PlayerEntry", "Navigating to player details for ID: ${player.id}")
                 navController.navigate("playerDetail/${player.id}") }
@@ -148,6 +177,7 @@ fun PlayerEntry(
         Row(
             modifier = Modifier
                 .padding(8.dp)
+                .background(Color.White)
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -168,11 +198,18 @@ fun PlayerEntry(
             Column {
                 Text(
                     text = player.commonName ?: player.name, // Exibe commonName ou name se commonName for nulo
-                    style = MaterialTheme.typography.bodyLarge.copy(color = Color.Black)
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = Color(0, 64, 0),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 )
                 Text(
                     text = "Rating: ${player.rating}",
-                    style = MaterialTheme.typography.bodyMedium.copy(color = Color.Gray)
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        color = Color.DarkGray,
+                        fontSize = 18.sp
+                    )
                 )
             }
         }
